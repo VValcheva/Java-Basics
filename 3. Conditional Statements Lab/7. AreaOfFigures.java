1c33d280eb48ad1e4d4e7d46a747093e3dcbafcb
VValcheva <venetsiya.jorova@abv.bg>
2020-07-14 23:45:16 +0300

    add task

---
+++ 3. Conditional Statements Lab/7. AreaOfFigures.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AreaOfFigures {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String type = scanner.nextLine();
+        double area = 0.00;
+        if (type.equals("square")) {
+            double side = Double.parseDouble(scanner.nextLine());
+            area = side * side;
+        } else if (type.equals("rectangle")) {
+            double a = Double.parseDouble(scanner.nextLine());
+            double b = Double.parseDouble(scanner.nextLine());
+            area = a * b;
+
+        } else if (type.equals("circle")) {
+            double r = Double.parseDouble(scanner.nextLine());
+            area = Math.PI * r * r;
+        } else {
+            double a = Double.parseDouble(scanner.nextLine());
+            double h = Double.parseDouble(scanner.nextLine());
+            area = a * h / 2;
+        }
+        System.out.printf("%.3f", area);
+
+    }
+}
