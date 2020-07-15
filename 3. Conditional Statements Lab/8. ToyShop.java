91f766db8ab9893f655519e8b27ae5fdd7249a49
VValcheva <venetsiya.jorova@abv.bg>
2020-07-15 12:56:14 +0300

    add task

---
+++ 3. Conditional Statements Lab/8. ToyShop.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ToyShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double price = Double.parseDouble(scanner.nextLine());
+        int puzzles = Integer.parseInt(scanner.nextLine());
+        int dolls = Integer.parseInt(scanner.nextLine());
+        int bears = Integer.parseInt(scanner.nextLine());
+        int minions = Integer.parseInt(scanner.nextLine());
+        int trucks = Integer.parseInt(scanner.nextLine());
+        double profit = puzzles * 2.60 + dolls * 3.00 + bears * 4.10 + minions * 8.20 + trucks * 2.00;
+        int toysCount = puzzles + dolls + bears + minions + trucks;
+
+        if (toysCount >= 50) {
+            profit = profit * 0.75;
+        }
+        profit = profit * 0.90;
+        if (profit >= price) {
+            System.out.printf("Yes! %.2f lv left.", profit - price);
+        } else {
+            System.out.printf("Not enough money! %.2f lv needed.", price - profit);
+        }
+
+    }
+}
