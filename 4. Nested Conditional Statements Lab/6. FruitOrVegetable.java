809c7465c163c01529bdd1f02f8dddb9d7e4e4c8
VValcheva <venetsiya.jorova@abv.bg>
2020-07-23 18:22:23 +0300

    add task

---
+++ 4. Nested Conditional Statements Lab/6. FruitOrVegetable.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FruitOrVegetable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        boolean isFruit =
+                input.equals("banana")
+                        || input.equals("apple")
+                        || input.equals("kiwi")
+                        || input.equals("cherry")
+                        || input.equals("lemon")
+                        || input.equals("grapes");
+        boolean isVegetable =
+                input.equals("tomato")
+                        || input.equals("cucumber")
+                        || input.equals("pepper")
+                        || input.equals("carrot");
+
+        if (isFruit) {
+            System.out.println("fruit");
+        } else if (isVegetable) {
+            System.out.println("vegetable");
+        } else {
+            System.out.println("unknown");
+        }
+    }
+}
